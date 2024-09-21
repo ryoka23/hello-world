13272fc8d10dc0d8f34c3a847f071657d20ca8a7
Ryan Oka <ryanoka23@gmail.com>
2024-09-20 17:56:51 -0700

    upload new python files

---
+++ draw.py
@@ -0,0 +1,14 @@
+import turtle
+painter = turtle.Turtle()
+<<<<<<< HEAD
+
+painter.pensize(20)  
+
+for _ in range(18):
+	painter.forward(100)
+	painter.left(100)
+=======
+painter.forward(100)
+painter.left(90)
+>>>>>>> parent of 930cf6d (repeat 4 time with a for loop)
+turtle.done()
